0857f3145430a840d16279e73af1ceca11c900fb
Stan-21 <shung951@gmail.com>
2025-08-13 23:17:33 -0700

    Border detection

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,26 @@ export interface Tile {
   down?: Tile[];
   left?: Tile[];
   right?: Tile[];
+
+  colors?: Color[];
+  top_border?: Color[];
+  bottom_border?: Color[];
+  left_border?: Color[];
+  right_border?: Color[];
+}
+
+export class Color { // Color of a single Pixel
+  red: number;
+  green: number;
+  blue: number;
+  alpha: number;
+
+  constructor(red: number, green: number, blue: number, alpha: number) {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+    this.alpha = alpha;
+  }
 }
 
 ////////**** EDIT THIS STUFF ****////////
@@ -18,7 +38,7 @@ export interface Tile {
  * Given an array of tiles, update each tile's direction properties to indicate which tiles it can tile with in each direction.
  * @param tiles Array of Tile objects to update
  */
-export function updateTileNeighbors(tiles: Tile[]): void {
+export function updateTileNeighbors(tiles: Tile[], pixelColor: Uint8ClampedArray[]): void {
   // Tiles are arranged in a grid, but represented as a 1D array. Starting at index 0, the first tile is in the top-left corner 
   // and the last tile is in the bottom-right corner. A neighboring tile can be added like so:
   //
@@ -28,6 +48,121 @@ export function updateTileNeighbors(tiles: Tile[]): void {
   // in place so you can skip some calculations, but the implementation is entirely up to you. I'm just guessing here.
   
   // TODO: implement neighbor assignment logic here
+  filterBorder(tiles, pixelColor);
+  for (let j = 0; j < tiles.length; j++) {
+    let up = [];
+    let down = [];
+    let right = [];
+    let left = [];
+    for (let i = 0; i < tiles.length; i++) {
+      if (compareBorder(tiles[j].bottom_border!, tiles[i].top_border!, 0.8)) {
+        up.push(tiles[i]);
+      }
+      if (compareBorder(tiles[j].top_border!, tiles[i].bottom_border!, 0.8)) {
+        down.push(tiles[i]);
+      }
+      if (compareBorder(tiles[j].left_border!, tiles[i].right_border!, 0.8)) {
+        right.push(tiles[i]);
+      }
+      if (compareBorder(tiles[j].right_border!, tiles[i].left_border!, 0.8)) {
+        left.push(tiles[i]);
+      }
+    }
+    tiles[j].up = up;
+    tiles[j].down = down;
+    tiles[j].right = right;
+    tiles[j].left = left;
+  }
+}
+
+// IDEAS: SOME SORT OF PERCENT ERROR TO DETERMINE WHETHER IT CONNECTS
+// AVERAGING COLORS OUT AND THEN USING CERTAIN ERROR AMOUNT?
+
+// RULES:
+// IF MATCHES PIXEL FOR PIXEL IT MUST BE A MATCH
+// IF NEIGHBORING PIXEL IS THE SAME MAYBE A MATCH?
+// IF EVERY PIXEL IS THE SAME, BUT DIFFERENT ORDER POSSIBLY A MATCH? (CHECK DISTANCES BETWEEN PIXELS?) (IF PIXELS IS EXACTLY THE SAME IN REVERSE, PROLLY NOT A MATCH?)
+// 
+
+
+export function filterBorder(tiles: Tile[], pixelColor: Uint8ClampedArray[]): Color[] {
+  let filteredArray: Color[] = [];
+
+  var tileNum = 0;
+  let tileSize = parseInt(tileSizeInput.value, 10);
+  for (let i = 0; i < pixelColor.length; i++) {
+    for (let j = 4; j <= pixelColor[i].length; j+= 4) {
+      var color = new Color(pixelColor[i][j-4], pixelColor[i][j-3], pixelColor[i][j-2], pixelColor[i][j-1]);
+      filteredArray.push(color);
+      if (filteredArray.length == tileSize * tileSize) {
+        tiles[tileNum].colors = filteredArray;
+        tiles[tileNum].top_border = filteredArray.slice(0, tileSize);
+        tiles[tileNum].bottom_border = filteredArray.slice(-tileSize);
+        tiles[tileNum].left_border = getLeft(filteredArray);
+        tiles[tileNum].right_border = getRight(filteredArray);
+        tileNum++;
+        filteredArray = [];
+      }
+    }
+  }
+  return filteredArray;
+}
+
+export function getLeft(array: Color[]): Color[]{
+  let filter: Color[] = [];
+  let size = parseInt(tileSizeInput.value, 10)
+  for (let i = 0; i < size; i++) {
+    filter.push(array[i * size]);
+  }
+  return filter;
+}
+
+export function getRight(array: Color[]): Color[]{
+  let filter: Color[] = [];
+  let size = parseInt(tileSizeInput.value, 10)
+  for (let i = 0; i < size; i++) {
+    filter.push(array[i * size + (size - 1)]);
+  }
+  return filter;
+}
+
+export function compareBorder(array1: Color[], array2: Color[], minPercent: number): boolean{
+  if (isBorderTransparent(array1) || isBorderTransparent(array2)) {
+    return false;
+  }
+  let err = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (!(compareColor(array1[i], array2[i]))) {
+      //array1.sort((a, b) => a.red - b.red);
+      //array2.sort((a, b) => a.red - b.red);
+      //return secondCheck(array1, array2, 0.8);
+      if ((compareColor(array1[i], array2[i + 1])) || (compareColor(array1[i], array2[i - 1]))) {
+        err += 0.5;
+      } else {
+        err += 1;
+      }
+    }
+  }
+  return ((parseInt(tileSizeInput.value, 10) - err )/ parseInt(tileSizeInput.value, 10)) > minPercent;
+}
+
+export function isBorderTransparent(array: Color[]): boolean{
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].alpha != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function compareColor(color1: Color, color2: Color): boolean{
+  if (color1 == null || color2 == null) {
+    return false;
+  }
+  if ((color1.red == color2.red) && (color1.blue == color2.blue) && (color1.green == color2.green) && (color1.alpha == color2.alpha)) { // EVERYTHING IS THE SAME
+    return true;
+  }
+  return false;
 }
 
 
@@ -51,6 +186,7 @@ processBtn.onclick = async () => {
       const cols = Math.floor(img.width / tileSize);
       const rows = Math.floor(img.height / tileSize);
       const tiles: Tile[] = [];
+      const pixelColor: Uint8ClampedArray[] = []; // EDIT HERE (STAN)
       const tempCanvas = document.createElement('canvas');
       tempCanvas.width = tileSize;
       tempCanvas.height = tileSize;
@@ -69,6 +205,8 @@ processBtn.onclick = async () => {
             tileSize,
             tileSize
           );
+          const tilePixels = tempCtx?.getImageData(0, 0, tileSize, tileSize).data; // EDITS HERE (STAN)  
+          pixelColor.push(tilePixels!);
           const image = tempCanvas.toDataURL();
           tiles.push({
             id: y * cols + x,
@@ -76,10 +214,8 @@ processBtn.onclick = async () => {
           });
         }
       }
-
-      updateTileNeighbors(tiles);
-
-      // Display all tile images on screen
+      updateTileNeighbors(tiles, pixelColor);
+// Display all tile images on screen
       const tileGallery = document.getElementById('tileGallery') || document.createElement('div');
       tileGallery.id = 'tileGallery';
       tileGallery.innerHTML = '';
@@ -184,3 +320,4 @@ processBtn.onclick = async () => {
   };
   reader.readAsDataURL(file);
 };
+
